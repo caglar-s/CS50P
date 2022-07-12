d617666cead439e443c3542a3d27e6c4b566505d
caglar-s <61076046+caglar-s@users.noreply.github.com>
2022-07-12 20:36:36 +1000

    Delete playback.py

--- playback.py
+++
@@ -1,9 +0,0 @@
-#Playback Speed
-#prompts the user for input and then output that same input, replacing each space with ... (i.e., three periods)
-
-def main():
-    userinput = input("Please enter a string: ")
-    print(userinput.replace(" ","..."))
-
-main()
-    
