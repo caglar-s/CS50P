8766d981bc6145d2625f82f5b2b775d1fb545a50
caglar-s <61076046+caglar-s@users.noreply.github.com>
2022-07-12 20:35:57 +1000

    Add files via upload

---
+++ 0.Functions,Variables/indoor.py
@@ -0,0 +1,9 @@
+#Indoor voice
+#Prompt the user for input then output that same input in lowercase
+
+def main():
+    userinput = input("Please enter a string: ")
+    print(userinput.lower())
+
+main()
+    
