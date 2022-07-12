ac6244e5d4981af681d7544b48719277038a6436
caglar-s <61076046+caglar-s@users.noreply.github.com>
2022-07-12 20:36:25 +1000

    Delete indoor.py

--- indoor.py
+++
@@ -1,9 +0,0 @@
-#Indoor voice
-#Prompt the user for input then output that same input in lowercase
-
-def main():
-    userinput = input("Please enter a string: ")
-    print(userinput.lower())
-
-main()
-    
