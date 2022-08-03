6e33b9f3386795ce0bdb100dcd049829f9813319
Shane Caglar <61076046+caglar-s@users.noreply.github.com>
2022-08-03 19:31:47 +1000

    Add files via upload

--- 2.Loops/coke.py
+++ 2.Loops/coke.py
@@ -13,5 +13,6 @@ amount_due = 50
 while amount_due > 0:
     print("Amount due: ", amount_due)
     coin = int(input("Insert Coin: "))
-    amount_due -= coin
+    if coin in [25,10,5]:
+        amount_due -= coin
 print("Change Owed: ", abs(amount_due))
